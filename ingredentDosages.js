93bc83ac7afc7ad1fdf0d31fe70c717d3199bc08
Arushi Bhatia <ab752@duke.edu>
2021-02-20 15:27:57 -0500

    made data into json objects in javascript files for easy local access

---
+++ ingredentDosages.js
@@ -0,0 +1,66 @@
+const ingredentDosages = {
+    "Codeine Phosphate": {
+        "doseSizePerInterval": 60,
+        "dosesPerInterval": 1,
+        "interval": 4,
+        "maxiumumDaily": 240
+    },
+
+    "Acetaminophen": {
+        "doseSizePerInterval": 1000,
+        "dosesPerInterval": 1,
+        "interval": 6,
+        "maxiumumDaily": 4000
+        
+    },
+
+    "Fentanyl": {
+        "doseSizePerInterval": 0.1,
+        "dosesPerInterval": 2,
+        "interval": 4,
+        "maxiumumDaily": 0.6
+        
+    },
+
+    "Hydrocodone bitartrate": {
+        "doseSizePerInterval": 2.5,
+        "dosesPerInterval": 2,
+        "interval": 6,
+        "maxiumumDaily": 20
+    },
+
+    "Hydromorphone hydrochloride": {
+        "doseSizePerInterval": 2,
+        "dosesPerInterval": 2,
+        "interval": 6,
+        "maxiumumDaily": 24
+    },
+
+    "Meperidine hydrochloride": {
+        "doseSizePerInterval": 50,
+        "dosesPerInterval": 3,
+        "interval": 4,
+        "maxiumumDaily": 900
+    },
+
+    "Morphine": {
+        "doseSizePerInterval": 10,
+        "dosesPerInterval": 1,
+        "interval": 4,
+        "maxiumumDaily": 60
+    },
+
+    "Oxycodone hydrochloride": {
+        "doseSizePerInterval": 10,
+        "dosesPerInterval": 1,
+        "interval": 12,
+        "maxiumumDaily": 20
+    }
+
+
+
+
+
+
+
+}
